7e137a79db4349a3c5564f554952b8bfe5d83b23
FliegendeWurst <2012gdwu@web.de>
2016-10-10 21:18:04 +0200

    Fix error message for subreddit removal

--- js/script.js
+++ js/script.js
@@ -249,7 +249,7 @@ $(function () {
         else if ((window.location+"").indexOf("+"+$("#navboxSubreddit").text().substr(3)) == (window.location+"").lastIndexOf("+") && (window.location+"").lastIndexOf("+") > 0)
             window.location = (window.location+"").replace("+"+$("#navboxSubreddit").text().substr(3), "").replace("#", "");
         else
-            toastr.error("Unable to remove " + $("#navboxSubreddit").text().substr(3) + " (this shouldnt happen :(");
+            toastr.error("Unable to remove " + $("#navboxSubreddit").text().substr(3));
     };
 
     var initState = function () {
